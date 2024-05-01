14f6840144fa72873d14a3e4dbceace9c4752682
Tareq <tareqnmd@gmail.com>
2024-05-01 15:11:17 +0600

    react query

--- src/components/TodoArea.tsx
+++ src/components/TodoArea.tsx
@@ -1,12 +1,15 @@
+import { useQuery } from 'react-query';
+import { fetchTodos } from '../api/todo';
 import AddTodo from './AddTodo';
 import Todos from './Todos';
 
 const TodoArea = () => {
+	const { data } = useQuery('todos', fetchTodos);
 	return (
 		<main className="app-main">
 			<nav>
 				<strong>Todo List</strong>
-                <span>Todo Count</span>
+				<span>Todo Count : {data?.length}</span>
 			</nav>
 			<AddTodo />
 			<Todos />
